e9f45d13988cd703cc4a79078df8d4354602ea2c
sayeefrmoyen <develop.sayeefrm@gmail.com>
2019-03-20 13:31:49 -0400

    filter out non-furniture objects for detection

--- app/src/main/java/com/strawberryjulats/roomtips/DetectorActivity.java
+++ app/src/main/java/com/strawberryjulats/roomtips/DetectorActivity.java
@@ -56,6 +56,7 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
   private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/labelmap.txt";
   private static final DetectorMode MODE = DetectorMode.TF_OD_API;
   // Minimum detection confidence to track a detection.
+  private static final Set<String> TARGET_LABELS = new HashSet<String>(Arrays.asList("chair", "couch", "table", "desk", "lamp"));
   private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
   private static final boolean MAINTAIN_ASPECT = false;
   private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
@@ -212,6 +213,7 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
                 new LinkedList<Classifier.Recognition>();
 
             for (final Classifier.Recognition result : results) {
+	      if(!TARGET_LABELS.CONTAINS(result.getTitle())) continue
               final RectF location = result.getLocation();
               if (location != null && result.getConfidence() >= minimumConfidence) {
                 canvas.drawRect(location, paint);
