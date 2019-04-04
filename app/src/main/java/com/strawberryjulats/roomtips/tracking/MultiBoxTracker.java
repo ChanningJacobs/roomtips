df02a8d795056637f4634e7fad5b23239da94a72
sayeefrmoyen <develop.sayeefrm@gmail.com>
2019-04-04 14:32:54 -0400

    broken merge with channing and sayeef

--- app/src/main/java/com/strawberryjulats/roomtips/tracking/MultiBoxTracker.java
+++ app/src/main/java/com/strawberryjulats/roomtips/tracking/MultiBoxTracker.java
@@ -168,14 +168,6 @@ public class MultiBoxTracker {
       frameHeight = h;
       this.sensorOrientation = sensorOrientation;
       initialized = true;
-
-      if (objectTracker == null) {
-        String message =
-            "Object tracking support not found. "
-                + "See tensorflow/examples/android/README.md for details.";
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-        Log.e(TAG, message);
-      }
     }
 
     if (objectTracker == null) {
